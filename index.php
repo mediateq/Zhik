4672cec3d8874c81b64ab461708af38a2cf358ee
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-02 12:16:35 +0330

    start zhik project

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="1styles.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div class="contain">
+		<div class="hline"></div>
+		<div class="container">
+			<header class="header">
+				<div class="head">
+					<div class="logo"></div>
+					<div class="search"></div>
+				</div>
+				<nav class="menu"></nav>
+			</header>
+			<section class="banner">
+				<div class="slideshow"></div>
+				<div class="text"></div>
+			</section>
+			<section class="middle1">
+				<div class="first"></div>
+				<div class="second"></div>
+				<div class="third"></div>
+			</section>
+			<section class="middle2">
+				<div class="first"></div>
+				<div class="second"></div>
+				<div class="third"></div>
+			</section>
+		</div>
+		<footer class="contain">
+			<section class="footer container">
+				<div class="first">
+					<div class="copyright"></div>
+					<div class="menu"></div>
+				</div>
+				<div class="second">
+					<div class="header"></div>
+					<div class="text"></div>
+				</div>
+				<div class="third">
+					<div class="header"></div>
+					<div class="schub1">
+						<div class="fa"></div>
+						<div class="tw"></div>
+					</div>
+					<div class="schub2">
+						<div class="gp"></div>
+						<div class="rs"></div>
+					</div>
+				</div>
+			</section>
+		</footer>
+	</div>
+</body>
+</html>
