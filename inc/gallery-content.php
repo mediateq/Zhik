6b6170000a20763ca4be627655877d25f5bb79df
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-05 11:46:09 +0330

    ok position

--- inc/gallery-content.php
+++ inc/gallery-content.php
@@ -2,19 +2,36 @@
 				<div class="title"><p>گالری</p></div>
 				<div class="gallerypic">
 					<div class="first">
-						<div class="pic"></div>
-						<div class="tit"></div>
-						<div class="text"></div>
+						<div class="pic"><img src="" alt=""></div>
+						<div class="tit"><p></p></div>
+						<div class="text"><p></p></div>
 					</div>
 					<div class="second">
-						<div class="pic"></div>
-						<div class="tit"></div>
-						<div class="text"></div>
+						<div class="pic"><img src="" alt=""></div>
+						<div class="tit"><p></p></div>
+						<div class="text"><p></p></div>
 					</div>
 					<div class="third">
-						<div class="pic"></div>
-						<div class="tit"></div>
-						<div class="text"></div>
+						<div class="pic"><img src="" alt=""></div>
+						<div class="tit"><p></p></div>
+						<div class="text"><p></p></div>
 					</div>
+					<div class="badboy"></div>
+					<div class="first">
+						<div class="pic"><img src="" alt=""></div>
+						<div class="tit"><p></p></div>
+						<div class="text"><p></p></div>
+					</div>
+					<div class="second">
+						<div class="pic"><img src="" alt=""></div>
+						<div class="tit"><p></p></div>
+						<div class="text"><p></p></div>
+					</div>
+					<div class="third">
+						<div class="pic"><img src="" alt=""></div>
+						<div class="tit"><p></p></div>
+						<div class="text"><p></p></div>
+					</div>
+					<div class="badboy"></div>
 				</div>
 			</section>
