dd824e1b6bff9ff76f1844f0ae6972d333eaadd8
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-05 11:37:41 +0330

    set position

--- inc/gallery-content.php
+++ inc/gallery-content.php
@@ -1,3 +1,20 @@
 			<section class="gallery">
-		
+				<div class="title"><p>گالری</p></div>
+				<div class="gallerypic">
+					<div class="first">
+						<div class="pic"></div>
+						<div class="tit"></div>
+						<div class="text"></div>
+					</div>
+					<div class="second">
+						<div class="pic"></div>
+						<div class="tit"></div>
+						<div class="text"></div>
+					</div>
+					<div class="third">
+						<div class="pic"></div>
+						<div class="tit"></div>
+						<div class="text"></div>
+					</div>
+				</div>
 			</section>
