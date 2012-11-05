18990fa5983076f3e457869368a11b494e91ca3d
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-05 10:51:50 +0330

    set position

--- inc/project-content.php
+++ inc/project-content.php
@@ -1,5 +1,8 @@
 			<section class="project">
-				<div class="title"><p></p></div>	
-				<div class="pic"><img src="" alt=""></div>
-				<div class="text"><p></p></div>
+				<div class="title"><p>پروژه ها</p></div>	
+				<div class="textpic">
+					<div class="text"><p></p></div>
+					<div class="pic"><img src="" alt=""></div>
+					<div class="badboy"></div>
+				</div>		
 			</section>
