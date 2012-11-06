ce38f60ea316dd7cfd9857b72a288a912d6981fc
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-06 17:05:39 +0330

    add code post type for news

--- single-news.php
+++ single-news.php
@@ -1,5 +1,30 @@
 <?php
 	get_template_part('./inc/header');
-	get_template_part('./inc/news-content');
+?>
+				
+				<section class="news">
+				<div class="title"><p>اخبار</p></div>
+				<?php
+					while (have_posts()) {
+						the_post();
+					
+				?>	
+
+					<div class="textpic">
+						<div class="text">
+							<div class="tit"><h2><?php the_title(); ?></h2></div>
+							<div class="date"><?php the_date(); ?></div>
+							<div class="desc"><p><?php the_content(); ?></p></div>
+						</div>
+						<div class="pic"><?php the_post_thumbnail(); ?></div>
+						<div class="badboy"></div>
+					</div>
+
+				<?php
+					}
+				?>	
+			</section>
+
+<?php
 	get_template_part('./inc/footer');
 ?>
