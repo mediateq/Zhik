5ba077a7cb2aa8b5339a071a26d647eb4df5741d
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-06 10:23:28 +0330

    add label for post type

--- functions.php
+++ functions.php
@@ -3,6 +3,23 @@
 
 	add_theme_support('post-thumbnails');
 
-	add_action('init','article_init')
+	add_action('init','article_init');
+
+	function article_init(){
+		$labels = array(
+		'name' => _x('مقالات','post type general name'),
+		'singular_name' => _x('مقاله','post type singular name'),
+		'add_new' => _x('اضافه نمودن مقاله' , 'News'),
+		'add_new_item' => __('اضافه نمودن مقاله'),
+		'edit_item' => __('اصلاح مقاله'),
+		'new_item' => __('مقاله جدبد'),
+		'view_item' => __('مشاهده مقاله'),
+		'search_items' => __('جستجوی مقاله'),
+		'not_found' => __('هیچ مقاله ای یافت نشد'),
+		'not_found_in_trash' => __('هیچ مقاله ای در زباله دان یافت نشد'),
+		'parent_item_colon' => '',
+		'menu_name' => 'مقالات'
+	);
+	}
 
 ?>
