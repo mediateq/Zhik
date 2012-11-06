ad031d4dfe55925e6b178422e74c89a7036b3a08
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-06 16:57:04 +0330

    add news post type

--- functions.php
+++ functions.php
@@ -67,3 +67,61 @@
 	}
 
 //////////////////////////// NEWS page ////////////////////////////////
+	add_theme_support('post-thumbnails');
+
+	add_action('init','news_init');
+
+	function news_init(){
+		$labels = array(
+		'name' => _x('اخبار','post type general name'),
+		'singular_name' => _x('خبر','post type singular name'),
+		'add_new' => _x('اضافه نمودن خبر' , 'News'),
+		'add_new_item' => __('اضافه نمودن خبر'),
+		'edit_item' => __('اصلاح خبر'),
+		'new_item' => __('خبر جدبد'),
+		'view_item' => __('مشاهده خبر'),
+		'search_items' => __('جستجوی خبر'),
+		'not_found' => __('هیچ خبری یافت نشد'),
+		'not_found_in_trash' => __('هیچ خبری در زباله دان یافت نشد'),
+		'parent_item_colon' => '',
+		'menu_name' => 'اخبار'
+	);
+		$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'has_archive' => true,
+		'hierarchical' => false,
+		'menu_position' => 5,
+		'menu_icon' => get_bloginfo('template_url') . '/images/newsicon.png',
+		'supports' => array('title','editor','thumbnail','excerpt','comments'),
+	);
+
+		register_post_type('news', $args);
+	}
+
+	add_shortcode('news', 'all_news');
+
+	function all_news(){
+		$news = new WP_Query(array(
+		'post_type' => 'news'
+	));
+
+		$html = "";
+
+		while($news->have_posts()){
+			$news->the_post();
+
+			$title   = get_the_title();
+			$content = get_the_content();
+
+			$html 	.= "";
+		}
+
+		return $html;
+	}
