b62c0a1a14eb84f6ea27c04df89e222b5ee8d0b7
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-06 17:14:21 +0330

    fix somth

--- single-article.php
+++ single-article.php
@@ -5,16 +5,15 @@
 				<div class="title"><h2>مطالب خواندنی</h2></div>
 
 				<?php
-
-				
 					while (have_posts()) {
 						the_post();
-					
 				?>
+				
 					<div class="tit"><p><?php the_title(); ?></p></div>	
 					<div class="text">
 						<p><?php the_content(); ?></p>
 					</div>
+
 				<?php
 					}
 				?>
