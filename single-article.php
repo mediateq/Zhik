0f47270c977639caff8b42c6bd5d8d700f5fb485
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-06 11:16:48 +0330

    fix err

--- single-article.php
+++ single-article.php
@@ -1,16 +1,19 @@
 <?php
-/*
-	Template Name: article
-*/
 	get_template_part('./inc/header');
 ?>
 			<section class="article">
+				<div class="title"><h2>مطالب خواندنی</h2></div>
+
 				<?php
+
+					$article = new WP_Query(array(
+						'post_type' => 'article'
+					));
+
 					while (have_posts()) {
 						the_post();
 					
 				?>
-					<div class="title"><h2>مطالب خواندنی</h2></div>
 					<div class="tit"><p><?php the_title(); ?></p></div>	
 					<div class="text">
 						<p><?php the_content(); ?></p>
