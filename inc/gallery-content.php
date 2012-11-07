a640bbfeb5337622d05be63c234941cc2d5852a9
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-07 16:45:02 +0330

    create gallery page

--- inc/gallery-content.php
+++ inc/gallery-content.php
@@ -7,8 +7,43 @@
 
 			<div class="gallerypic">
 
+				<?php
+					$gall_attach= array(
+								'numberposts' => '-1',
+								'orderby '=> 'menu_order',  
+								'order'=> 'ASC',  
+								'post_mime_type' => 'image', 
+								'post_parent' => $post->ID, 
+								'post_status' => null, 
+								'post_type' => 'attachment'
+					);
+
+					$images= get_children($gall_attach);
+
+					if ($images){
+
+						foreach ($images as $img) {
+
+							echo "<div class='pic'>";
+
+								$img_med  = wp_get_attachment_image($img->ID,'medium');
+								$img_larg = wp_get_attachment_image_src($img->ID,'large');
+								
+								echo "<a rel='' href=\"$img_larg[0]\" title=\"$img->post_content\"> $img_med </a>";
+
+								echo "<div class='text'><p> $img->post_content </p></div>";
+							
+							echo "</div>";
+						}
+						echo "<div class='badboy'></div>";
+
+						
+
+					}else echo "<p class='no-image'>تصویری در گالری یافت نشد</p>";
+				?>
+
 			</div>
-			
+
 		<?php
 			}
 		?>
