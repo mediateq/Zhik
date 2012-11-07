5d4d88146cea4e601b8979c4a9d8273497744ac0
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-07 16:31:24 +0330

    strat to create gallery page

--- inc/gallery-content.php
+++ inc/gallery-content.php
@@ -1,65 +1,15 @@
-			<section class="gallery">
-				<div class="title"><p>گالری</p></div>
-				<div class="gallerypic">
-					<div class="first">
-						<div class="pic"><img src="<?php bloginfo('template_url'); ?>/images/gallery/gal1.jpg" alt=""></div>
-						<div class="tit"><p>عکس 1</p></div>
-						<div class="text">
-							<p>
-								گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1گالری 1
-							</p>
-						</div>
-					</div>
-					<div class="second">
-						<div class="pic"><img src="<?php bloginfo('template_url'); ?>/images/gallery/gal2.jpg" alt=""></div>
-						<div class="tit">
-							<p>
-								عکس 2
-							</p>
-						</div>
-						<div class="text">
-							<p>
-								گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2گالری 2
-							</p>
-						</div>
-					</div>
-					<div class="third">
-						<div class="pic"><img src="<?php bloginfo('template_url'); ?>/images/gallery/gal3.jpg" alt=""></div>
-						<div class="tit"><p>عکس 3</p></div>
-						<div class="text">
-							<p>
-								گالری 3
-							</p>
-						</div>
-					</div>
-					<div class="badboy"></div>
-					<div class="first">
-						<div class="pic"><img src="<?php bloginfo('template_url'); ?>/images/gallery/gal4.jpg" alt=""></div>
-						<div class="tit"><p>عکس 4</p></div>
-						<div class="text">
-							<p>
-								گالری 4
-							</p>
-						</div>
-					</div>
-					<div class="second">
-						<div class="pic"><img src="<?php bloginfo('template_url'); ?>/images/gallery/gal5.jpg" alt=""></div>
-						<div class="tit"><p>عکس 5</p></div>
-						<div class="text">
-							<p>
-								گالری 5
-							</p>
-						</div>
-					</div>
-					<div class="third">
-						<div class="pic"><img src="<?php bloginfo('template_url'); ?>/images/gallery/gal6.jpg" alt=""></div>
-						<div class="tit"><p>عکس 6</p></div>
-						<div class="text">
-							<p>
-								گالری 6
-							</p>
-						</div>
-					</div>
-					<div class="badboy"></div>
-				</div>
-			</section>
+	<section class="gallery">
+		<div class="title"><p>گالری</p></div>
+		<?php
+			if (have_posts()) {
+				the_post();
+		?>
+
+			<div class="gallerypic">
+
+			</div>
+			
+		<?php
+			}
+		?>
+	</section>
--- index-gallery.php
+++ index-gallery.php
@@ -1,4 +1,7 @@
 <?php
+/*
+		Template Name: gallery
+*/
 	get_template_part('./inc/header');
 	get_template_part('./inc/gallery-content');
 	get_template_part('./inc/footer');
