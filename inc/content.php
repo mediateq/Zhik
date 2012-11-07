f15f504adc0789cb09214c7785762eb41771839e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-07 20:37:16 +0330

    add codes for set images

--- inc/content.php
+++ inc/content.php
@@ -5,7 +5,28 @@
 				?>
 					<div class="slideshow">
 						<ul id="slider">
-							
+							<?php
+								$slide_attach= array(
+									'numberposts' => '-1',
+									'orderby '=> 'menu_order',  
+									'order'=> 'ASC',  
+									'post_mime_type' => 'image', 
+									'post_parent' => $post->ID, 
+									'post_status' => null, 
+									'post_type' => 'attachment'
+								);
+
+								$images= get_children($slide_attach);
+
+								if ($images){
+
+									foreach ($images as $img) {
+										
+										$img_larg= wp_get_attachment_image($img->ID,'large');
+										echo "<li> $img_larg </li>";
+									}
+								}
+							?> 
 						</ul>
 					</div>
 				<div class="text">
