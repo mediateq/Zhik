1f6808a0840c1355a019b9036b4d27bf0c72c2f7
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-11-10 10:01:00 +0330

    add style to aboutUs page

--- inc/about-content.php
+++ inc/about-content.php
@@ -1,16 +1,14 @@
 			<section class="about">
-						<?php
-							if (have_posts()) {
-								the_post();
-			
-						?>
-					<div class="title"><p><?php the_title(); ?></p></div>	
+				<?php
+					if (have_posts()) {
+						the_post();
+				?>	
 					<div class="text">
 						<p>
 							<?php the_content(); ?>
 						</p>
 					</div>
-					<?php
-						}
-					?>
+				<?php
+					}
+				?>
 			</section>
